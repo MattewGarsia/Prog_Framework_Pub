c92f6a57d97d570bd828b84117cddd9224415552
MattewGarsia <96680650+MattewGarsia@users.noreply.github.com>
2026-03-26 22:14:11 +0100

    Delete framework_ricerca_spazio_stati directory

--- framework_ricerca_spazio_stati/graphProblem/graphAction.cpp
+++
@@ -1,9 +0,0 @@
-#include "graphAction.hpp"
-
-graphAction::graphAction(State* dest_state_in, double cost_in):  destination_state(dest_state_in), cost(cost_in){
-
-}
-
-double graphAction::getCost() const{
-    return cost;
-}
